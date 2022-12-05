a9b317708d7ffece64564cad49059407d0ec8513
Kishan Suvarna <kishansuvarna09@gmail.com>
2022-12-05 10:35:12 +0530

    uncomment the validation

--- src/components/ContactMe/ContactMe.jsx
+++ src/components/ContactMe/ContactMe.jsx
@@ -76,35 +76,31 @@ const ContactMe = () => {
 
   const sendEmail = async () => {
     setErrors({});
-    setResponse({
-      success: 'Yusss YOU DID IT! Sam will get back to you soo..oon.',
-    });
-    return false;
     const validate = await validateInput(templateParams);
     if (!isEmptyObject(validate)) {
       setErrors(validate);
       return false;
     }
-    // await emailjs
-    //   .send(
-    //     import.meta.env.VITE_EMAILJS_SERVICE_ID,
-    //     import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
-    //     templateParams,
-    //     import.meta.env.VITE_EMAILJS_PUBLIC_KEY
-    //   )
-    //   .then(
-    //     (result) => {
-    //       console.log('SUCCESS!', result.status, result.text);
-    //       setResponse({
-    //         success:
-    //           'Yusss YOU DID IT! Sam will get back to you soo..oon.',
-    //       });
-    //     },
-    //     (error) => {
-    //       console.log(error.text);
-    //       setResponse({ error: 'OOPS! Failed to send the message' });
-    //     }
-    //   );
+    await emailjs
+      .send(
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        templateParams,
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          console.log('SUCCESS!', result.status, result.text);
+          setResponse({
+            success:
+              'Yusss YOU DID IT! Sam will get back to you soo..oon.',
+          });
+        },
+        (error) => {
+          console.log(error.text);
+          setResponse({ error: 'OOPS! Failed to send the message' });
+        }
+      );
   };
 
   return (
