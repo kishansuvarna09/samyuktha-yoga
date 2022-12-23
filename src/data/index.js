7781b415d53876db0ff11b1e790a4ec2feedde31
Kishan Suvarna <kishansuvarna09@gmail.com>
2022-12-23 19:45:11 +0530

    updated content

--- src/data/index.js
+++ src/data/index.js
@@ -1,6 +1,6 @@
 const content = {
   contentIntro:
-    '<p>I am Samyuktha and I am a 200hr Yoga certified teacher based in Bangalore. For many years, Yoga has been a huge part of my life and it is now my passion to share the Yogi magic with as many people as I can. I provide virtual classes for all levels which includes Hatha and ashtanga style yoga.</p><br/><p>Whether you are looking to start your Yoga journey, take some time for yourself or fancy a good workout, I am here to guide you through!</p><br/><p>Feel free to message me if you have any questions or would like to know more, I look forward to seeing you on the mat.</p><br/><p>Namaste x</p>',
+    '<p>I am Samyuktha and I am a 200hr Yoga certified teacher based in Bangalore. For many years, Yoga has been a huge part of my life and it is now my passion to share the Yogi magic with as many people as I can. I provide virtual classes for all levels which includes Hatha and Ashtanga Style Yoga.</p><br/><p>Whether you are looking to start your Yoga journey, take some time for yourself or fancy a good workout, I am here to guide you through!</p><br/><p>Feel free to message me if you have any questions or would like to know more, I look forward to seeing you on the mat.</p><br/><p>Namaste x</p>',
 };
 
 export default content;
