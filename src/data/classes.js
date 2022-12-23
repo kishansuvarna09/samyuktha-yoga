f92790c6c31b9dfc3260ff3378a4f53e1b74075a
Kishan Suvarna <kishansuvarna09@gmail.com>
2022-12-23 19:46:31 +0530

    updated content

--- src/data/classes.js
+++ src/data/classes.js
@@ -9,7 +9,7 @@ const classes = [
     title: 'BEGINNERS/INTERMEDIATE YOGA',
     subtitle: '12-Day Program',
     description:
-      'Join me in a virtual setting for a 60 Minute Hatha style flow, helping to welcome Yoga into the body and the mind! This session integrates yoga poses and breathwork and is perfect for anyone starting their Yoga journey!',
+      'Join me in a virtual setting for a 60 Minute Hatha style flow, helping to welcome Yoga into the body and the mind! This session integrates Yoga poses and breathwork and is perfect for anyone starting their Yoga journey!',
     timings: [
       'Monday, Wednesday, Friday - (8am to 9am)',
       'Tuesday, Thursday, Friday - (8pm to 9pm)',
@@ -21,7 +21,7 @@ const classes = [
     title: 'VINYASA - (Intermediate/Advanced)',
     subtitle: '8-Day Program',
     description:
-      'Join me for an Energising Vinyasa style flow for 60 Mins, helping to bring strength to the body and a release for the mind.',
+      'Join me for an Energising Vinyasa style flow for 60 minutes, helping to bring strength to the body and a release for the mind.',
     timings: ['Saturday, Sunday - (8am to 9am)'],
     image: VinyasaImage,
   },
@@ -30,8 +30,8 @@ const classes = [
     title: 'Pranayama/breathe work - All Levels',
     subtitle: '8-Day Program',
     description:
-      'Relax and De-stress with me in a soothing pranayama class suitable for all levels and for anyone wanting some time out for self care!',
-    timings: ['Saturday, Sunday - (45 mins)'],
+      'Relax and De-stress with me in a soothing Pranayama class suitable for all levels and for anyone wanting some time out for self care!',
+    timings: ['Saturday, Sunday - (45 minutes)'],
     image: PrivateImage,
   },
   {
@@ -39,7 +39,7 @@ const classes = [
     title: 'Weekend Workshop - All Levels',
     subtitle: '2-Day Program',
     description:
-      'Join me on the weekend workshop where you will be nourished and guided through gentle restorative yoga postures, receive hands-on energy healing, and then gather in a circle for intention setting. Please bring a journal, pen and a hand towel.',
+      'Join me on the weekend workshop where you will be nourished and guided through gentle restorative Yoga postures, receive hands-on energy healing, and then gather in a circle for intention setting. Please bring a journal, pen and a hand towel.',
     image: WeekendWorkshopImage,
     timings: ['To be scheduled'],
   },
